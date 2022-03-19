e155f47b2f9798b1b972d9570c29885359c61826
Dana <danazaripova@icloud.com>
2022-03-19 20:30:40 +0300

    Task 2

---
+++ Project_Bars/Project_Bars/CharWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Bars
+{
+    /// <summary>
+    /// Считывает символ с консоли.
+    /// </summary>
+    class KeyReader 
+    {
+        /// <summary>
+        /// Обработчик нажатия клавиши.
+        /// </summary>
+        public event EventHandler<char> OnKeyPressed;
+
+        /// <summary>
+        /// Считывает введенный в консоли символ в цикле.
+        /// </summary>
+        public void Run()
+        {
+            for(;;)
+            {
+                Console.Write("Введите символ: ");
+                var ch = Console.ReadKey();
+                if (ch.Key == ConsoleKey.C)
+                {
+                    break;
+                }
+                
+                OnKeyPressed?.Invoke(this, ch.KeyChar);
+            }
+        }
+    }
+}
---
+++ Project_Bars/Project_Bars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_Bars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charWriter = new KeyReader();
+            charWriter.OnKeyPressed += (sender, ch) => Console.WriteLine("\n" + ch);
+            charWriter.Run();
+        }
+    }
+}
